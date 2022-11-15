07ce3f0fd8f8739f12afa8a59a3e2c8c0595b8e3
Vazgen Levonyan <vazgenlevonyan1898@gmail.com>
2022-11-16 01:20:12 +0400

    Refactore add and  modify all structure of the game

--- java/snack/Snack.java
+++ java/snack/Snack.java
@@ -1,20 +1,43 @@
+import java.util.Scanner;
+
 public class Snack {
 
-    private static char[] returnMap(char[] arr, int point) {
-        char swap = arr[point];
-        arr[point] = arr[point + 3];
-        arr[point + 3] = swap;
-        return arr;
+    private static char[] createEmptyMap(int mapLength) {
+        char[] map = new char[mapLength];
+
+        for (int i = 0; i < mapLength; i++) {
+            map[i] = '-';
+        }
+        return map;
     }
 
-    public static void main(String[] args) {
+    private static void printMap(char[] emptyMap) {
 
-        char[] map = {'*', '*', '*', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'};
-
-        for (int i = 0; i < map.length; i++) {
-            System.out.print(map[i]);
+        for (int i = 0; i < emptyMap.length; i++) {
+            System.out.print(emptyMap[i] + " ");
         }
+    }
 
+    private static char[] createSnack(int length) {
+
+        char[] snack = new char[length];
+        for (int i = 0; i < length; i++) {
+            snack[i] = '*';
+        }
+        return snack;
+    }
+
+    private static char[] insertSnackInMap(char[] map, char[] snack, int position) {
+        if (snack.length + position > map.length) System.out.println("Please pay attention to map borders");
+
+        for (int i = 0; i < snack.length; i++) {
+            map[position] = snack[i];
+            position++;
+        }
+        return map;
+    }
+
+    private static int findFirstSnackPosition(char[] map) {
         int firstPoint = -1;
 
         for (int i = 0; i < map.length; i++) {
@@ -23,29 +46,60 @@ public class Snack {
                 break;
             }
         }
+        return firstPoint;
+    }
 
-        long startTime = System.nanoTime();
-        System.out.println();
-        System.out.println(startTime);
+    private static void moveSnackOneStepToForward(char[] map, int snackLength) {
+        int firstSnackPosition = findFirstSnackPosition(map);
+        if (firstSnackPosition + snackLength >= map.length)
+            throw new RuntimeException("Snack can not move forward as it will go out of bounds");
 
-        for (int i = 0; i < map.length - 3; i++) {
-            char swap = map[firstPoint];
-            map[firstPoint] = map[firstPoint + 3];
-            map[firstPoint + 3] = swap;
-            firstPoint++;
+        char currentPosition = map[firstSnackPosition];
+        map[firstSnackPosition] = map[firstSnackPosition + snackLength];
+        map[firstSnackPosition + snackLength] = currentPosition;
+    }
+
+    private static void moveSnackOneStepToBackward(char[] map, int snackLength) {
+        int firstSnackPosition = findFirstSnackPosition(map);
+        if (firstSnackPosition == 0)
+            throw new RuntimeException("Snack can not move backward as it will go out of bounds");
+
+        char currentPosition = map[firstSnackPosition - 1];
+        map[firstSnackPosition - 1] = map[firstSnackPosition + snackLength - 1];
+        map[firstSnackPosition + snackLength - 1] = currentPosition;
+    }
+
+    private static void moveSnackOnMap(char[] map, String direction, int snackLength) {
+        if (direction.equals("f")) {
+            moveSnackOneStepToForward(map, snackLength);
+        } else if (direction.equals("b")) {
+            moveSnackOneStepToBackward(map, snackLength);
+        } else {
+            return;
         }
+    }
 
-        long endTime = System.nanoTime();
 
-        System.out.println(endTime);
+    public static void main(String[] args) {
 
-        long time = endTime - startTime;
+        int mapSize = 10;
+        int snackSize = 3;
 
-        for (int i = 0; i < map.length; i++) {
-            System.out.print(map[i]);
-        }
+        char[] emptyMap = createEmptyMap(mapSize);
+        printMap(emptyMap);
         System.out.println();
-
-        System.out.println(time);
+        char[] snack = createSnack(snackSize);
+        char[] snackInMap = insertSnackInMap(emptyMap, snack, 7);
+        printMap(snackInMap);
+        System.out.println();
+        while (true) {
+            System.out.println();
+            System.out.println("Please input 'b' or 'f'");
+            System.out.println();
+            Scanner scanner = new Scanner(System.in);
+            String direction = scanner.next();
+            moveSnackOnMap(snackInMap, direction, snackSize);
+            printMap(snackInMap);
+        }
     }
 }
