b82cb1f60c35f73e32bbaf0123d8b782cdf856fc
davekhach <davaramkhach@gmail.com>
2022-11-22 15:20:42 +0400

    Add my first snake game

---
+++ src/MethodsForSnake.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MethodsForSnake {
+    public static int map() {
+        int map = 15;
+        return map;
+    }
+
+    public static int snake() {
+        int snake = 3;
+        return snake;
+    }
+
+    public static int startPosition() {
+        int start = 0;
+        return start;
+    }
+
+    public static int movement() {
+        Scanner scanner = new Scanner(System.in);
+        String direction = scanner.next().toLowerCase();
+        int move = 0;
+        if (direction.equals("f")) {
+            move = +1;
+        } else if (direction.equals("b")) {
+            move = -1;
+        } else {
+            System.out.println("Please type valid command");
+        }
+        return move;
+    }
+
+    public static void endOfTheGame(int current) {
+        int map = map();
+        int snake = snake();
+        if (current < (map - snake)) {
+            System.out.println("Sorry, you lose");
+        } else if (current > (map - snake)) {
+            System.out.println("Congrats, you WIN");
+        }
+    }
+}
---
+++ src/SnakeBegins.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SnakeBegins {
+
+    public static void main(String[] args) {
+        //some code
+        System.out.println("Game is starting...\n press <f> to forward, <b> to backward");
+        int current = MethodsForSnake.startPosition();
+        int map = MethodsForSnake.map();
+        int snake = MethodsForSnake.snake();
+        char[] mapLength = new char[map];
+        char[] snakeLength = new char[snake];
+        while(current <= (map - snake) && current >= 0) {
+            Arrays.fill(mapLength, '-');
+            for (int i = current; i < snakeLength.length + current; i++) {
+                mapLength[i] = '*';
+            }
+            System.out.println(mapLength);
+            current = current + MethodsForSnake.movement();
+        }
+        MethodsForSnake.endOfTheGame(current);
+    }
+}
