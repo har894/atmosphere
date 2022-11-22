c3b2bfc2e2b04519570b6c0f5fac8aee078b654e
arammkhitaryan <mkhitaryan.aram.93@gmail.com>
2022-11-22 11:07:43 +0400

    Refactor Game class

--- src/das/src/das/Game.java
+++ src/das/src/das/Game.java
@@ -1,36 +1,52 @@
 package src.das;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Game {
-    static String[] swap(String[] strings, int n, int m) {
-        for (int j = 0; j < strings.length; j++) {
-            String temp = strings[m];
-            strings[m] = strings[n];
-            strings[n] = temp;
+    static String[] swap(String[] snakeArray, int firstElement, int secondElement) {
+        for (int j = 0; j < snakeArray.length; j++) {
+            String temp = snakeArray[secondElement + 2];
+            snakeArray[secondElement + 2] = snakeArray[firstElement];
+            snakeArray[firstElement] = temp;
+            break;
         }
-        return strings;
+        return snakeArray;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the length of the array:");
+        int lengthOfArray = scanner.nextInt();
 
+        String[] firstSnake = new String[lengthOfArray];
+        System.out.println("Enter the elements of the array:");
 
-        String[] strings = new String[5];
-        strings[0] = "*";
-        strings[1] = "-";
-        strings[2] = "-";
-        strings[3] = "-";
-        strings[4] = "-";
-
-        int M = 0;
-        for (int k = 0; k < strings.length - 1; k++) {
-            M++;
-            String[] secondString = swap(strings, k, M);
-            for (int i = 0; i < secondString.length; i++)
-                System.out.print(secondString[i] + " ");
-            System.out.println("\n");
+        for (int i = 0; i < lengthOfArray; i++) {
+            firstSnake[i] = scanner.next();
         }
 
+        System.out.println(Arrays.toString(firstSnake));
+        System.out.println("\n");
+
+        System.out.println("Game is start:");
+
+        for (int n = 0; n < firstSnake.length; n++) {
+            System.out.print(firstSnake[n] + " ");
+        }
+        System.out.println("\n");
+
+        int M = 0;
+        for (int k = 0; k < firstSnake.length - 3; k++) {
+            M++;
+            String[] secondSnake = swap(firstSnake, k, M);
+            for (int i = 0; i < secondSnake.length; i++)
+
+                System.out.print(secondSnake[i] + " ");
+            Thread.sleep(350);
+            System.out.println("\n");
+        }
+        System.out.println("Finish!");
     }
 
 }
