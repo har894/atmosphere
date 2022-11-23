a174c4799183ed5dcf96a48f712f1039a2e51606
sevakivanyan <sevakivanyan@gmail.com>
2022-11-23 12:32:04 +0400

    Add info logs

--- java/snake/Main.java
+++ java/snake/Main.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static final Logger log = Logger.getLogger("logs");
+
+    public static void main(String[] args) throws IOException {
+
+
+        log.info("Loge processes started");
+        FileHandler logFile;
+        logFile = new FileHandler("./infoLogFile.txt");
+        log.addHandler(logFile);
+
+        log.info("Game started");
 
         System.out.println("please input number of game board length");
         Scanner scannerLength = new Scanner(System.in);
@@ -11,15 +25,19 @@ public class Main {
 
         final int SNAKE_STARTING_X = SCREEN_WIDTH / 2;
 
+        log.info("Add game screen");
         GameScreen screen = new GameScreen(SCREEN_WIDTH);
         screen.InitScreen();
 
+        log.info("Snake element one");
         Snake snake = new Snake('*', SNAKE_STARTING_X);
         screen.setObjectOnLocation(snake, snake.getX());
 
+        log.info("Snake element two");
         Snake snake1 = new Snake('*', SNAKE_STARTING_X + 1);
         screen.setObjectOnLocation(snake1, snake1.getX());
 
+        log.info("Snake element three");
         Snake snake2 = new Snake('*', SNAKE_STARTING_X + 2);
         screen.setObjectOnLocation(snake2, snake2.getX());
 
@@ -31,11 +49,13 @@ public class Main {
             screen.PrintScreen();
             switch (input = scanner.nextLine().charAt(0)) {
                 case 'l':
+                    log.info("Snake move left");
                     snake.moveLeft(screen, snake);
                     snake1.moveLeft(screen, snake1);
                     snake2.moveLeft(screen, snake2);
                     break;
                 case 'r':
+                    log.info("Snake move right");
                     snake2.moveRight(screen, snake2);
                     snake1.moveRight(screen, snake1);
                     snake.moveRight(screen, snake);
