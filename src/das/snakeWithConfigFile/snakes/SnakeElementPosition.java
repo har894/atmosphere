57c9dfa760a2a6ec517cdd367a99f8f1ec0312ff
arammkhitaryan <mkhitaryan.aram.93@gmail.com>
2022-11-23 13:35:13 +0400

    Refactored the SnakeElementPosition class

--- src/das/snakeWithConfigFile/snakes/SnakeElementPosition.java
+++ src/das/snakeWithConfigFile/snakes/SnakeElementPosition.java
@@ -1,12 +1,23 @@
 package snakeWithConfigFile.snakes;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+
 public class SnakeElementPosition {
 
     public   int snackElementPosition(char[] map) {
+        ReadFileIntoList readFileIntoList = new ReadFileIntoList();
+        List<String> listOfConfigWithStringValue = readFileIntoList.readFileInList("/Users/arammkhitaryan/Desktop/snack/atmosphere/src/das/snakeWithConfigFile/snakes/configForStringValue.text");
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        for (int i = 0; i < listOfConfigWithStringValue.size(); i++) {
+            stringStringHashMap.put((listOfConfigWithStringValue.get(i).substring(0, listOfConfigWithStringValue.get(i).indexOf(':'))), listOfConfigWithStringValue.get(i).substring(listOfConfigWithStringValue.get(i).lastIndexOf(":") + 1));
+        }
+        String snakeSymbol = stringStringHashMap.get("snakeSymbol");
         int firstPoint = -1;
 
         for (int i = 0; i < map.length; i++) {
-            if (map[i] == '*') {
+            if (map[i] == snakeSymbol.charAt(0)) {
                 firstPoint = i;
                 break;
             }
