c9280b7d8f70d8b7e3e55a36c31e29d5e605d22f
lianagasparyan <liana.gaspariann@gmail.com>
2022-12-06 05:59:25 -0800

    Check is the inserted number is prime and find the prime numbers between 2 and 100

---
+++ trainingqt.Snack/trainingqt/Snake/ParzTiveGtnel.java
@@ -0,0 +1,41 @@
+package trainingqt.Snake;
+
+
+import java.util.Scanner;
+
+public class ParzTiveGtnel {
+    public static void main(String[] args) {
+        int number;
+        int count=0;
+//option 1
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number you want");
+        number= scanner.nextInt();
+        for(int i=2;i<=number/2;i++){
+            if (number%2==0){
+                count++;
+            }
+        }
+        if(count==0 && number!=1){
+            System.out.println(number+" Is a Prime");
+
+        }
+        else {
+            System.out.println(number+" Is not Prime");
+        }
+
+        //Option 2
+        for (int i=2;i<=100;i++){
+            for (int j=2;j<=i;j++){
+                if (j==i){
+                    System.out.println(i);
+                }
+                if (i%j==0){
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
